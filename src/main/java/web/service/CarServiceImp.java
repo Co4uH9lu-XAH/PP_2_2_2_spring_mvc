000f396f15fc09e809a35bf273217933f0cbde0b
Co4uH9lu-XAH <eugenkunin@gmail.com>
2023-02-10 22:02:25 +0300

    one more time for check

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -2,7 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import web.DAO.CarsDAO;
+import web.dao.CarsDAO;
 import web.model.Car;
 
 import java.util.List;
